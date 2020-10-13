8134fbe9e76e0b8d0e6cd2725b468b88d4e2f968
hanseungtheshiba <ggm.t200019@ggh.goe.go.kr>
2020-10-13 14:28:25 +0900

    10-13 수업 내용 업데이트

---
+++ Assets/Scripts/Base/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Player
+{
+    public long calcium = 0;
+    public float jeinHeight = 154.4f;
+    public Doll[] dolls;
+}
+
+[Serializable]
+public class Doll 
+{
+    public long level = 1;
+    public float purchaseCost = 10f;
+    public float upgradeCost = 0.1f;
+    public long calciumPerSecond = 10;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+
+    private string jsonString = "";
+    public void OnClickSave()
+    {
+        jsonString = JsonUtility.ToJson(player);
+        Debug.Log("JSON : " + jsonString);
+    }
+}
